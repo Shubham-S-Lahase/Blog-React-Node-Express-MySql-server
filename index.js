36e7e1a634e610f9ab4da6cc83bbdf5a0881c895
Shubham <shubhamlahase@hmail.com>
2023-07-30 12:44:48 +0530

    register and login endpoints done and tested on frontend

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ const express = require("express");
 const postRoutes = require("./routes/posts");
 const userRoutes = require("./routes/users");
 const authRoutes = require("./routes/auth");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 
 
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/auth", authRoutes);
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
