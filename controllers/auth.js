2afd1910a4a3bef8a3c599ab9122462250836452
Shubham <shubhamlahase@hmail.com>
2023-07-31 10:20:32 +0530

    user logout funnctionality route added

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,28 +3,26 @@ const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 
 const register = (req, res) => {
+  const q = "SELECT * FROM users WHERE email = ? OR username = ?";
 
-   const q = "SELECT * FROM users WHERE email = ? OR username = ?";
+  db.query(q, [req.body.email, req.body.username], (err, data) => {
+    if (err) return res.status(500).json(err);
+    if (data.length) return res.status(409).json("User already exists!");
 
-   db.query(q, [req.body.email, req.body.username], (err, data) => {
-     if (err) return res.status(500).json(err);
-     if (data.length) return res.status(409).json("User already exists!");
- 
-     const salt = bcrypt.genSaltSync(10);
-     const hash = bcrypt.hashSync(req.body.password, salt);
- 
-     const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
-     const values = [req.body.username, req.body.email, hash];
- 
-     db.query(q, [values], (err, data) => {
-       if (err) return res.status(500).json(err);
-       return res.status(200).json("User has been created.");
-     });
-   });
-}
+    const salt = bcrypt.genSaltSync(10);
+    const hash = bcrypt.hashSync(req.body.password, salt);
+
+    const q = "INSERT INTO users(`username`,`email`,`password`) VALUES (?)";
+    const values = [req.body.username, req.body.email, hash];
+
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json("User has been created.");
+    });
+  });
+};
 
 const login = (req, res) => {
-
   const q = "SELECT * FROM users WHERE username = ?";
 
   db.query(q, [req.body.username], (err, data) => {
@@ -42,11 +40,21 @@ const login = (req, res) => {
     const token = jwt.sign({ id: data[0].id }, "jwtkey");
     const { password, ...other } = data[0];
 
-    res.cookie("access_token", token, {httpOnly: true,}).status(200).json(other);});
-}
+    res
+      .cookie("access_token", token, { httpOnly: true })
+      .status(200)
+      .json(other);
+  });
+};
 
 const logout = (req, res) => {
-    res.json("logout");
-}
+  res
+    .clearCookie("access_token", {
+      sameSite: "none",
+      secure: true,
+    })
+    .status(200)
+    .json("User has been logged out.");
+};
 
-module.exports = {register, login, logout};
+module.exports = { register, login, logout };
