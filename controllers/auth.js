6e5261159d91e8712b8cd3a5b9d915eeb2d5bc8f
Shubham Lahase <shubhamlahase@gmail.com>
2024-07-25 10:55:45 +0530

    cookies logout

--- controllers/auth.js
+++ controllers/auth.js
@@ -61,8 +61,8 @@ const login = (req, res) => {
 const logout = (req, res) => {
   res
     .clearCookie("access_token", {
-      sameSite: "none",
-      secure: true,
+      httpOnly: true,
+      sameSite: "None",
     })
     .status(200)
     .json("User has been logged out.");
